0b7b0984e496543720aa78900a319d3aabb59168
Alstn1012 <kimminsu101220062@gmail.com>
2024-01-04 16:42:55 +0900

    랜덤 맵 생성 때 필요한 스크립트와 타일 (미완성)

---
+++ ZEandRG's project/Assets/Script/SpreadTilemap.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SpreadTilemap : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap floor; //타일맵 오브젝트
+    [SerializeField]
+    private Tilemap wall; //타일맵 오브젝트
+    [SerializeField]
+    private Tilemap floorTile; //사용할 타일 에셋
+    [SerializeField]
+    private Tilemap wallTile;  //사용할 타일 에셋
+
+    public void SpreadFloorTilemap (HashSet<Vector2Int> positions)
+    {
+        SpreadTile(positions, floor, floorTile);
+    }
+
+    public void SpreadWallTilemap(HashSet<Vector2Int> positions)
+    {
+        SpreadTile(positions, wall, wallTile);
+    }
+
+    public void SpreadTile(HashSet<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+    {
+        foreach (var position in positions)
+        {
+            tilemap.SetTile((Vector3Int) position, tile);
+        }
+    }
+
+    public void ClearAllTiles()
+    {
+        floor.ClearAllTiles();
+        wall.ClearAllTiles();
+    }
+}
