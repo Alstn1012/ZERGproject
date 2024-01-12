defc8c9a16a25722eba02e77073f0ca436f97a66
Alstn1012 <kimminsu101220062@gmail.com>
2024-01-12 19:34:35 +0900

    랜덤 맵 생성 초기 완성

---
+++ ZEandRG's project/Assets/Script/ObjectArrayExample.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectArrayExample : MonoBehaviour
+{
+    public string prefabTag = "Room";
+    public int arraySize = 8;
+
+    private GameObject[] gameObjectsArray;
+
+    private void Start()
+    {
+        gameObjectsArray = new GameObject[arraySize];
+
+        // 배열에 태그를 기반으로 게임 오브젝트 할당
+        for (int i = 0; i < arraySize; i++)
+        {
+            string tagName = prefabTag + (i + 1); // "Room1", "Room2", ..., "Room8" 등의 태그를 하는거에용
+            GameObject obj = GameObject.FindGameObjectWithTag(tagName);
+
+            if (obj != null)
+            {
+                gameObjectsArray[i] = obj;
+            }
+            else
+            {
+                Debug.LogError("GameObject with tag '" + tagName + "' not found!");
+            }
+        }
+
+        int randomIndex = Random.Range(0, arraySize);
+
+        gameObjectsArray[randomIndex].transform.position = new Vector2(0, 0);
+    }
+}
