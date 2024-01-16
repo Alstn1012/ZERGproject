03ab680dab0de69de2f98b311e6983dda8178e52
sungtae <129718219+ainnot@users.noreply.github.com>
2024-01-17 01:04:33 +0900

    플레이어 관련 스크립트 추가
    playerStatus,playerCollision등 플레이어 충돌처리와 플레이어 상태를 제어하는 스크립트 추가

---
+++ ZEandRG's project/Assets/sungtae's folder/playerStatus.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStatus : MonoBehaviour
+{
+    public bool isAlive;
+    public bool whileInvincible;
+    public bool isDash;
+    public int playerHp=3;
+    public static playerStatus instance;
+    private void Awake()
+    {
+        if(playerStatus.instance == null)
+        {
+            playerStatus.instance = this;
+        }
+    }
+    void Start()
+    {
+        isAlive= true;
+        isDash= false;
+        whileInvincible= false;
+    }
+    void Update()
+    {
+        if (playerHp == 0)
+        {
+            isAlive= false;
+        }
+    }
+}
