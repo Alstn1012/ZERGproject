1b399020259ca1800f041e997566857c8f387223
Alstn1012 <kimminsu101220062@gmail.com>
2024-01-17 13:10:55 +0900

    플레이어 점프 구현

--- ZEandRG's project/Assets/Script/Player.cs
+++ ZEandRG's project/Assets/Script/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    public float jumpPower = 10.0f;
+    public bool isJump = false;
 
     void Start()
     {
@@ -14,9 +16,22 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // 플레이어 이동
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 moveDirection = new Vector2(horizontalInput, 0);
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpPower * 4.0f;
+        }
     }
 }
