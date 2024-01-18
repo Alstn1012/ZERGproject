923c6223edba1d691220d01fcc0a7c405918e0f0
sungtae <129718219+ainnot@users.noreply.github.com>
2024-01-18 15:50:08 +0900

    player
    임시 플레이어 이미지 추가 및 무적 시간,이펙트 추가

--- ZEandRG's project/Assets/player/Player.cs
+++ ZEandRG's project/Assets/player/Player.cs
@@ -8,8 +8,8 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpPower = 3f;
     public int jumpLimit = 2;
-    private Rigidbody2D rb;
     private int jumpCount = 0;
+    private Rigidbody2D rb;
 
     void Start()
     {
@@ -18,11 +18,11 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // �÷��̾� �̵�
+        // 플레이어 이동
         playerMove();
         playerJump();
     }
-    void playerMove()
+    void playerMove() {
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 moveDirection = new Vector2(horizontalInput, 0);
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
--- ZEandRG's project/Assets/player/playerCollision.cs
+++ ZEandRG's project/Assets/player/playerCollision.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class playerCollision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    public float invincibleTimeLimit=1f;
+    private float invincibleTime;
+    private float blinkTime;
+    private void blink(float limit)
+    {
+        if (blinkTime < limit/8.0f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - 0.6f);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
+            if (blinkTime > limit/4.0f)
+            {
+                blinkTime = 0;
+            }
+        }
+        blinkTime += Time.deltaTime;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "enemy" || collision.gameObject.tag == "bullet")
         {
-            Destroy(collision.gameObject);
-            playerStatus.instance.whileInvincible = true;
-            playerStatus.instance.playerHp -= 1;
+            if (playerStatus.instance.whileInvincible == false)
+            {
+                Destroy(collision.gameObject);
+                playerStatus.instance.playerHp -= 1;
+                playerStatus.instance.whileInvincible = true;
+            }
         }
-    }
-    void Start()
-    {
-        
     }
     void Update()
     {
-        
+        if (playerStatus.instance.whileInvincible== true)
+        {
+            if (invincibleTime < invincibleTimeLimit)
+            {
+                invincibleTime += Time.deltaTime;
+                blink(invincibleTimeLimit);
+            }
+            else
+            {
+                invincibleTime= 0;
+                playerStatus.instance.whileInvincible= false;
+            }
+        }
     }
 }
