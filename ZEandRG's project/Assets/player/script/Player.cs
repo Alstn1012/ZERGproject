99088e641b29c017980d2b08e9731efcc568fb31
ainnot <stkim2007@gmail.com>
2024-01-20 23:57:53 +0900

    add dash
    대쉬 추가(미완성)및 폴더정리

--- ZEandRG's project/Assets/player/script/Player.cs
+++ ZEandRG's project/Assets/player/script/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,9 +10,16 @@ public class Player : MonoBehaviour
     public float jumpPower = 3f;
     public int jumpLimit = 2;
     private int jumpCount = 0;
+
+    private bool canDash = true;
+    private float dashPower = 25f;
+    private float dashingTime = 0.2f;
+    private float dashCool = 1f;
+
     private Rigidbody2D rb;
     private SpriteRenderer rend;
     private Animator anim; // 에니메이터 변수 선언
+    [SerializeField] private TrailRenderer trail;
 
     void Start()
     {
@@ -22,8 +30,13 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (playerStatus.instance.isDash)
+        {
+            return;
+        }
         playerMove();
         playerJump();
+        playerDash();
 
         float horizontalInput = Input.GetAxis("Horizontal");
 
@@ -44,11 +57,11 @@ public class Player : MonoBehaviour
         Vector2 moveDirection = new Vector2(horizontalInput, 0);
         if (horizontalInput > 0)
         {
-            rend.flipX= false;
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         }
         else if(horizontalInput < 0)
         {
-            rend.flipX= true;
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
         }
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
     }
@@ -64,6 +77,13 @@ public class Player : MonoBehaviour
             }
         }
     }
+    void playerDash()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift)&&canDash)
+        {
+            StartCoroutine(dash());
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "floor")
@@ -71,5 +91,20 @@ public class Player : MonoBehaviour
             jumpCount = 0;
             anim.SetBool("jump", false); // 비 점프 상태 전환
         }
+    }
+    private IEnumerator dash()
+    {
+        canDash = false;
+        playerStatus.instance.isDash= true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
+        trail.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        trail.emitting = false;
+        rb.gravityScale = originalGravity;
+        playerStatus.instance.isDash= false;
+        yield return new WaitForSeconds(dashCool);
+        canDash= true;
     }
 }
